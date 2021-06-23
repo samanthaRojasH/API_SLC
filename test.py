c694fa38eac65232b0939469b317de27e718458c
Sandra Rojas Herran <sandra.rojas.herran@gmail.com>
2021-06-22 22:43:23 -0500

    New Token

---
+++ test.py
@@ -0,0 +1,22 @@
+s="Python" 
+print ("\n\n")
+print (s[0])#Output:P
+print (s[-1])#Output:n
+print (s[-2])#Output:o
+print ("\n\n")
+list_=[10,20,30,40,50]
+print (list_[0])#Output:10
+print (list_[-1])#Output:50
+print (list_[-2])#Output:40
+
+#Indexing - tuple
+t=(1,2,3,4,5)
+print (t[0])#Output:1
+print (t[-1])#Output:5
+print (t[-2])#Output:4
+
+#Indexing - range object
+r=range(5)
+print (r[0])#Output:0
+print (r[-1])#Output:4
+print (r[-2])#Output:3
